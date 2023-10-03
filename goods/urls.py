f344c17b274be9b225f1721c1da25ec99622d846
Tarasidze <dtar84@ukr.net>
2023-10-03 22:06:03 +0300

    add Article CRUD and endpoints

--- goods/urls.py
+++ goods/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from goods.views import CategoryViewSet
+from goods.views import CategoryViewSet, ArticleViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register("category", CategoryViewSet)
+router.register("article", ArticleViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets, mixins, status
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAdminUser
-from rest_framework.viewsets import GenericViewSet
+from django.db.models.query import QuerySet
 from goods.models import Article, Category
-from goods.serializers import CategorySerializer
+
+from goods.serializers import (
+    CategorySerializer,
+    ArticleSerializer,
+    ArticleListSerializers,
+    ArticleDetailSerializer,
+
+)
 
 
 class CategoryViewSet(
@@ -15,3 +22,42 @@ class CategoryViewSet(
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAdminUser,)
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = (Article.objects.all().select_related("category"))
+    serializer_class = ArticleSerializer
+    permission_classes = (IsAdminUser,)
+
+    def get_queryset(self) -> QuerySet:
+        """ filtering by Category """
+        category_id = self.request.query_params.get("category")
+        offer_of_the_month = self.request.query_params.get("offer_of_the_month")
+        available = self.request.query_params.get("available")
+        self_delivery = self.request.query_params.get("self_delivery")
+
+        queryset = self.queryset
+
+        if category_id:
+            queryset = queryset.filter(category__id=int(category_id))
+
+        if offer_of_the_month:
+            queryset = queryset.filter(offer_of_the_month=offer_of_the_month)
+
+        if available:
+            queryset = queryset.filter(available=available)
+
+        if self_delivery:
+            queryset = queryset.filter(self_delivery=self_delivery)
+
+        return queryset
+
+    def get_serializer_class(self):
+        """ get serializer depends on request"""
+        if self.action == "list":
+            return ArticleListSerializers
+
+        if self.action == "retrieve":
+            return ArticleDetailSerializer
+
+        return ArticleSerializer
